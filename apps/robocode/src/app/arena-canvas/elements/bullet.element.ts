438bd8eef71a9fa376a94312b8b84a361bf812d1
DrDreo <code@anderas-hahn.at>
2024-08-01 23:21:17 +0200

    scale bullets

--- apps/robocode/src/app/arena-canvas/elements/bullet.element.ts
+++ apps/robocode/src/app/arena-canvas/elements/bullet.element.ts
@@ -14,22 +14,25 @@ export class BulletElement implements DrawableElement {
         this.y = data.position.y;
     }
 
-    draw(context: CanvasRenderingContext2D): void {
-        this.drawBody(context);
-
-        // Reset transformation matrix to the identity matrix
-        context.setTransform(1, 0, 0, 1, 0, 0);
+    draw(ctx: CanvasRenderingContext2D): void {
+        this.drawBody(ctx);
     }
 
-    private drawBody(context: CanvasRenderingContext2D) {
-        context.beginPath();
-        context.fillStyle = "#352918";
+    private drawBody(ctx: CanvasRenderingContext2D) {
+        ctx.save();
         const offset = this.size / 2;
         const posX = this.x + offset;
-        const posY = context.canvas.height - this.y + offset;
-        context.arc(posX, posY, this.size, 0, 2 * Math.PI, false);
-        context.fill();
+        const posY = ctx.canvas.height - this.y + offset;
 
-        context.closePath();
+        ctx.translate(posX, posY);
+
+        ctx.beginPath();
+        ctx.fillStyle = "#352918";
+
+        ctx.arc(0, 0, this.size, 0, 2 * Math.PI, false);
+        ctx.fill();
+
+        ctx.closePath();
+        ctx.restore();
     }
 }
